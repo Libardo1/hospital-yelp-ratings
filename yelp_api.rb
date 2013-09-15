d8bcc79524d2203cec21b905b8df65bf8446d9c1
semerj <jsemer@gmail.com>
2013-09-14 20:05:22 -0700

    Update yelp_api.rb

--- yelp_api.rb
+++ yelp_api.rb
@@ -15,10 +15,26 @@ access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
 zip = []
 path = []
+
 for i in 0..2652 do
 	zip = CSV.read('/Users/jsemer/Dropbox/yelp/ca_zip.csv')[i][0]
 	path = "/v2/search?location=" + zip + "&category_filter=hospitals"
-	File.open("/Users/jsemer/Dropbox/yelp/" + zip + ".json","w") do |f| 
-		f.write(access_token.get(path).body.to_json)
+	parsed = JSON.parse(access_token.get(path).body)
+	parsed["businesses"].each do |hosp|
+		CSV.open("/Users/jsemer/Dropbox/yelp/test/" + zip + ".csv", "ab") do |csv|
+		 	csv << [hosp['id'], 
+				 	hosp['is_claimed'], 
+				 	hosp['is_closed'], 
+				 	hosp['name'], 
+				 	hosp['review_count'], 
+				 	hosp['categories'].shift.shift,
+				 	hosp['rating'], 
+				 	hosp['snippet_text'], 
+				 	hosp['location']['address'][0],
+				 	hosp['location']['address'][1],
+				 	hosp['location']['city'],
+				 	hosp['location']['state_code'], 
+				 	hosp['location']['postal_code']]
+		end
 	end
 end
