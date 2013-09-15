5fcf8dea4287eb9a65556a6ad8a9d2c611f2d96e
semerj <jsemer@gmail.com>
2013-09-14 20:06:05 -0700

    Update yelp_api.rb

--- yelp_api.rb
+++ yelp_api.rb
@@ -22,19 +22,19 @@ for i in 0..2652 do
 	parsed = JSON.parse(access_token.get(path).body)
 	parsed["businesses"].each do |hosp|
 		CSV.open("/Users/jsemer/Dropbox/yelp/test/" + zip + ".csv", "ab") do |csv|
-		 	csv << [hosp['id'], 
-				 	hosp['is_claimed'], 
-				 	hosp['is_closed'], 
-				 	hosp['name'], 
-				 	hosp['review_count'], 
-				 	hosp['categories'].shift.shift,
-				 	hosp['rating'], 
-				 	hosp['snippet_text'], 
-				 	hosp['location']['address'][0],
-				 	hosp['location']['address'][1],
-				 	hosp['location']['city'],
-				 	hosp['location']['state_code'], 
-				 	hosp['location']['postal_code']]
+		 	csv << [ hosp['id'], 
+				 hosp['is_claimed'], 
+				 hosp['is_closed'], 
+				 hosp['name'], 
+				 hosp['review_count'], 
+				 hosp['categories'].shift.shift,
+				 hosp['rating'], 
+				 hosp['snippet_text'], 
+				 hosp['location']['address'][0],
+				 hosp['location']['address'][1],
+				 hosp['location']['city'],
+				 hosp['location']['state_code'], 
+				 hosp['location']['postal_code'] ]
 		end
 	end
 end
